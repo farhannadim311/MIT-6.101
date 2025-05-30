ecdfedd23692226ee75f6b07ae9ec4da02e52f3f
farhannadim311 <121982005+farhannadim311@users.noreply.github.com>
2025-05-30 12:59:21 -0500

    Update lab.py

--- bacon/lab.py
+++ bacon/lab.py
@@ -31,8 +31,26 @@ def acted_together(transformed_data, actor_id_1, actor_id_2):
     return actor_id_2 in transformed_data.get(actor_id_1, set())
 
 def actors_with_bacon_number(transformed_data, n):
-    raise NotImplementedError("Implement me!")
-
+    bacon_set = set()
+    idx = 0
+    result = [transformed_data.get(4724, set())]
+    explored = [4724]
+    if (n == 0):
+        return 4724
+    if (n == 1):
+        return transformed_data.get(4724, frozenset())
+    for i in range(n):
+        for k in result[i]:
+            if (k in explored):
+                pass
+            else:
+                result.append(transformed_data.get(k, frozenset()))
+                explored.append(k)
+    explored = set(explored)
+    for i in result:
+        bacon_set = bacon_set ^ i
+    bacon_set = bacon_set - explored
+    return bacon_set
 
 def bacon_path(transformed_data, actor_id):
     raise NotImplementedError("Implement me!")
@@ -53,12 +71,7 @@ def actors_connecting_films(transformed_data, film1, film2):
 if __name__ == "__main__":
     with open("resources/small.pickle","rb") as f:
         db = pickle.load(f)
-    with open("resources/names.pickle","rb") as f:
-        name = pickle.load(f)
-    actor_id1 = name["Joseph McKenna"]
-    actor_id2 = name["Dean Paraskevopoulos"]
-    data = transform_data(db)
-    print(acted_together(data, actor_id1, actor_id2))
+    print(db)
     # additional code here will be run only when lab.py is invoked directly
     # (not when imported from test.py), so this is a good place to put code
     # used, for example, to generate the results for the online questions.
