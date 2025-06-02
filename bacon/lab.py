bc48ddec8e21dda032f80a2629d917a2a8dfd326
farhannadim311 <121982005+farhannadim311@users.noreply.github.com>
2025-06-02 10:21:16 -0500

    Update lab.py

--- bacon/lab.py
+++ bacon/lab.py
@@ -13,65 +13,170 @@ import pickle
 
 
 def transform_data(raw_data):
-    """
-    Transforming the list into a set of tuples for efficiency
-    """
     graph = dict()
-    for a1,a2, _ in raw_data:
+    for a1,a2, m in raw_data:
+        if a1 not in graph:
+            graph[a1] = []
+        if a2 not in graph:
+            graph[a2] = []
+        if str(m) not in graph:
+            graph[str(m)] = []
+        if a2 not in graph[a1] and a2 != a1:
+            graph[a1].append(a2)
+        if a1 not in graph[a2] and a1 != a2:
+            graph[a2].append(a1) 
+        if str(m) not in graph[a1]: 
+            graph[a1].append(str(m))
+        if str(m) not in graph[a2]:  
+            graph[a2].append(str(m))   
+        if a1 not in graph[str(m)]:    
+            graph[str(m)].append(a1)  
+        if a2 not in graph[str(m)]:    
+            graph[str(m)].append(a2)  
+    return graph
+
+
+def movie_helper(raw_data):
+    graph = dict()
+    for a1, a2, m in raw_data:
         if a1 not in graph:
             graph[a1] = set()
         if a2 not in graph:
             graph[a2] = set()
-        graph[a1].add(a2)
-        graph[a2].add(a1)
+        graph[a1].add(m)
+        graph[a2].add(m)
     return graph
+
+def actors(transformed_data, actor):
+    lst = transformed_data[actor]
+    ret = []
+    for k in lst:
+        if (isinstance(k, int)):
+            ret.append(k)
+    return ret
+
+def movies(transformed_data, movie):
+    lst = transformed_data[str(movie)]
+    ret = []
+    for k in lst:
+        ret.append(k)
+    return ret
+
+def actors_in_movies(transformed_data, actor):
+    lst = transformed_data[actor]
+    ret = []
+    for k in lst:
+        if (isinstance(k, str)):
+            ret.append(int(k))
+    return ret
+
 def acted_together(transformed_data, actor_id_1, actor_id_2):
     if(actor_id_1 == actor_id_2):
         return True
     return actor_id_2 in transformed_data.get(actor_id_1, set())
 
 def actors_with_bacon_number(transformed_data, n):
-    bacon_set = set()
-    idx = 0
-    result = [transformed_data.get(4724, set())]
-    explored = [4724]
-    if (n == 0):
-        return 4724
-    if (n == 1):
-        return transformed_data.get(4724, frozenset())
-    for i in range(n):
-        for k in result[i]:
-            if (k in explored):
-                pass
-            else:
-                result.append(transformed_data.get(k, frozenset()))
-                explored.append(k)
-    explored = set(explored)
-    for i in result:
-        bacon_set = bacon_set ^ i
-    bacon_set = bacon_set - explored
-    return bacon_set
+    if n == 0:
+        return {4724}
+    
+    visited = set([4724])
+    queue = [(4724, 0)]  
+    result = set()
+
+    while queue:
+        actor, depth = queue.pop(0)  
+        if depth == n:
+            result.add(actor)
+        elif depth < n:
+            for neighbor in actors(transformed_data, actor):
+                if neighbor not in visited:
+                    visited.add(neighbor)
+                    queue.append((neighbor, depth + 1))
+    
+    return result
 
 def bacon_path(transformed_data, actor_id):
-    raise NotImplementedError("Implement me!")
+    return actor_path(transformed_data, 4724, lambda x: x == actor_id)
 
 
 def actor_to_actor_path(transformed_data, actor_id_1, actor_id_2):
-    raise NotImplementedError("Implement me!")
-
+    return actor_path(transformed_data, actor_id_1, lambda x: x == actor_id_2)
 
 def actor_path(transformed_data, actor_id_1, goal_test_function):
-    raise NotImplementedError("Implement me!")
+    if(goal_test_function(actor_id_1)):
+        return [actor_id_1]
+    visited = set([actor_id_1])
+    possible_paths = [(actor_id_1, )]
+    path = []
+    while(possible_paths):
+        path = possible_paths.pop(0)
+        curr_loc = path[-1]
+        if (goal_test_function(curr_loc)):
+            return path
+        for neighbor in actors(transformed_data, curr_loc):
+            if neighbor not in visited:
+                visited.add(neighbor)
+                possible_paths.append(path + (neighbor,))
+    return None
 
 
+def movie_path(transformed_data, actor_id_1, actor_id_2, movie_data):
+    if(actor_id_1 == actor_id_2):
+        return movie_data[actor_id_1]
+    path = actor_to_actor_path(transformed_data, actor_id_1, actor_id_2)
+    if(path == None):
+        return None
+    path = list(path)
+    movie_visited = set()
+    movie = []
+    while(path):
+        a1 = path.pop(0)
+        if a1 == actor_id_2:
+            return movie
+        s = movie_data[a1]
+        t = next(iter(s))
+        if t not in movie_visited:
+            movie_visited.add(t)
+            movie.append(t)
+    if (len(movie) == 0):
+        return None
+    return movie_path
+    
+    
 def actors_connecting_films(transformed_data, film1, film2):
-    raise NotImplementedError("Implement me!")
+    if (str(film1) not in transformed_data or str(film2) not in transformed_data):
+        return None
+    if(film1 == film2):
+        return transformed_data[str(film1)][0]
+    path = []
+    movie_visited = set()
+    possible_paths = [(transformed_data[str(film1)], )]
+    while(possible_paths):
+        path = possible_paths.pop(0)
+        curr_loc = path[-1]
+        lst = actors_in_movies(transformed_data, curr_loc)
+        for k in lst:
+            for neighbor in movies(transformed_data, k):
+                if neighbor not in movie_visited:
+                    movie_visited.add(neighbor)
+                    possible_paths.append(path + (neighbor, ))
+                if neighbor == film2:
+                    return path
+    return None
+    
 
 
 if __name__ == "__main__":
-    with open("resources/small.pickle","rb") as f:
-        db = pickle.load(f)
-    print(db)
+
+    # Load raw data
+    with open("resources/tiny.pickle", "rb") as f:
+        raw_data = pickle.load(f)
+
+    print(raw_data)
+
+
+    
+
     # additional code here will be run only when lab.py is invoked directly
     # (not when imported from test.py), so this is a good place to put code
     # used, for example, to generate the results for the online questions.
